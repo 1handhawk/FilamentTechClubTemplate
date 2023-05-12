b8ff9b7426227462d17db5e9a9b8b6d0c744cd79
1handhawk <133279532+1handhawk@users.noreply.github.com>
2023-05-12 17:58:20 -0500

    not ready properly

--- css/scripts/script.js
+++ css/scripts/script.js
@@ -1,4 +1,20 @@
 
 function changeColor() {
+    var currentColor = document.getElementById("tristanhawk").style.color;
+    var newColor = selectNewColor(currentColor);
     document.getElementById("tristanhawk").style.color = "red"
 }
+
+
+function selectNewColor(currentColor) { 
+    var newColor = "";
+
+    if (currentColor === "") { 
+        newColor = "red";  
+    } else if (currentColor === "red") {
+        newColor = "purple";
+    } else if (currentColor === "purple") {
+        newColor = "green";
+    } else if (currentColor === "green") {
+    }
+    }
